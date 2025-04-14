45e7e5b06e1da4bc3972f4f1473df3feed075f59
Volodymyr <volodymyr.yurg@gmail.com>
2025-04-14 18:05:37 +0300

    fixed proj naming and folders structure

---
+++ DigitalLibrary.App/Program.cs
@@ -0,0 +1,15 @@
+﻿using DigitalLibrary.Core;
+
+Console.WriteLine("app is working");
+var book = new Book()
+{
+    Id = 1,
+    Name = "C# in Depth",
+    Author = "Jon Skeet",
+    Year = 2019
+};
+
+Console.WriteLine($"Book ID: {book.Id}");
+Console.WriteLine($"Book Name: {book.Name}");
+
+Console.ReadLine();
